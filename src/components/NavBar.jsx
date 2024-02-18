2c7177f26bfafac3e235ff5b570e12c42dde6afe
jospin6 <jospinndagano1@gmail.com>
2024-02-18 19:38:41 +0200

    Design the title on navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,8 @@
 
 
 export function NavBar () {
-    return <div className="navbar">
-
+    return <div className="w-full h-[80px] flex justify-between border-b-[1px] border-[#3B3B3B] px-[50px]">
+        <div className="text-white leading-[80px] text-[25px]">filmFusion</div>
+        <div></div>
     </div>
 }
