01abf69b65c9615bcbaa47eea5af9a47fe959676
jospin6 <jospinndagano1@gmail.com>
2024-02-19 13:04:05 +0200

    Add behaviour to the search icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import search from "../assets/search.png";
+import Close from "../assets/Close.png";
+import { SearchBar } from "./SearchBar";
 
 export function NavBar () {
-    return <div className="w-full h-[80px] flex justify-between border-b-[1px] border-[#3B3B3B] px-[50px]">
-        <div className="text-white leading-[80px] text-[25px]">filmFusion</div>
-        <div className="h-[80px] flex items-center justify-center">
-            <img src={search} alt="Search" />
+    const [isHidden, setHidden] = useState(false)
+    
+    const hideSearchbar = () => {
+        setHidden(!isHidden)
+    }
+
+    return <div>
+        <div className="w-full h-[80px] flex justify-between border-b-[1px] border-[#3B3B3B] px-[50px]">
+            <div className="text-white leading-[80px] text-[25px]">filmFusion</div>
+            <div className="h-[80px] flex items-center justify-center">
+                {isHidden 
+                    ? <img src={search} onClick={hideSearchbar} alt="Search" />
+                    : <img src={Close} onClick={hideSearchbar} alt="Close" />
+                }
+            </div>
         </div>
+        {!isHidden && <SearchBar hidden/>}
     </div>
 }
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,9 @@
+
+
+export function SearchBar () {
+    return <div className="text-gray-500 h-[40px] border-b-[1px] border-[#3B3B3B] px-[30px] 
+    leading-[40px] flex justify-between">
+        <div>Recherche...</div>
+        <div>x</div>
+    </div>
+}
