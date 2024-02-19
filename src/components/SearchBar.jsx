7f7fb04f2712e095fc7511592259728f44750eb0
jospin6 <jospinndagano1@gmail.com>
2024-02-19 14:35:42 +0200

    Write test for the SearchBar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,13 @@
 
+import PropTypes from 'prop-types';
 
-export function SearchBar () {
-    return <div className="text-gray-500 h-[40px] border-b-[1px] border-[#3B3B3B] px-[30px] 
+export const SearchBar = ({search, close}) => (<div className="text-gray-500 h-[40px] border-b-[1px] border-[#3B3B3B] px-[30px] 
     leading-[40px] flex justify-between">
-        <div>Recherche...</div>
-        <div>x</div>
+        <div> { search } </div>
+        <div> { close } </div>
     </div>
+);
+SearchBar.propTypes = {
+    search: PropTypes.string.isRequired,
+    close: PropTypes.string.isRequired
 }
