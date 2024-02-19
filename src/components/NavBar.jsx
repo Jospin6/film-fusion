837f63acf2a9b85f95dfb49d747ab1a91a6882fe
jospin6 <jospinndagano1@gmail.com>
2024-02-19 10:53:07 +0200

    Add the search icon to the navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,10 @@
-
+import search from "../assets/search.png";
 
 export function NavBar () {
     return <div className="w-full h-[80px] flex justify-between border-b-[1px] border-[#3B3B3B] px-[50px]">
         <div className="text-white leading-[80px] text-[25px]">filmFusion</div>
-        <div></div>
+        <div className="h-[80px] flex items-center justify-center">
+            <img src={search} alt="Search" />
+        </div>
     </div>
 }
