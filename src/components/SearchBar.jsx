0d5172ae1673dc39b5b8fc31f8c347521a230b32
jospin6 <jospinndagano1@gmail.com>
2024-02-27 16:37:02 +0200

    Design Search item

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,28 @@
-import { useState } from 'react';
 import { Input } from './Input';
 import PropTypes from 'prop-types';
+import {queryField, setQuery} from '../slices/searchFilm'
+import {useSelector, useDispatch} from 'react-redux'
+import { useEffect } from 'react';
 
 export const SearchBar = ({placeholder, close}) => {
+    const inputValue = useSelector(queryField)
+    const dispatch = useDispatch()
 
-    const [search, setSearch] = useState('');
-    const handleChange = (e) => setSearch(e.target.value);
-    const resetSearch = () => setSearch('');
+    const handleChange = (e) => dispatch(setQuery(e.target.value))
+    const resetSearch = () => {
+        dispatch(setQuery(''))
+    }
 
     return <div className="text-gray-500 h-[40px] border-b-[1px] border-gray-300 px-[10px] md:px-[30px] 
     leading-[40px] flex justify-between overflow-hidden">
-        <div className='w-[95%]'> <Input onChange={handleChange} search={search} placeholder={placeholder}/> </div>
-        <div className='w-[5%] text-end'> <button className='cursor-pointer text-[20px] px-4' onClick={resetSearch}>{ close }</button> </div>
+        <div className='w-[95%]'> 
+            <Input onChange={handleChange} search={inputValue} placeholder={placeholder}/> 
+        </div>
+        <div className='w-[5%] text-end'> 
+            <button className='cursor-pointer text-[20px] px-4' onClick={resetSearch}>
+                { close }
+            </button>
+        </div>
     </div>
 
 };
---
+++ src/components/SearchedFilmsItems.jsx
@@ -0,0 +1,25 @@
+import {imageUrl, longText, emptyArray} from '../helpers/index'
+import { Link } from 'react-router-dom'
+
+export const SearchedFilm = (props) => {
+    const {films} = props
+    return <div className="w-full h-auto py-2">
+        {films.map(film => 
+            (
+                <Link to={`/singleFilm/${film.id}`} key={film.id} onClick={emptyArray(films)}
+                className="h-[150px] rounded-lg mb-4 itemShadow flex mx-4">
+                    <div className="h-full w-[100px] rounded-l-lg">
+                        <img src={imageUrl(film.poster_path)} alt={film.title} className='h-full w-[100px] rounded-l-lg' />
+                    </div>
+                    <div className='px-4 pt-2'>
+                        <div className='md:text-[20px] text-[18px] font-bold'> {film.title} </div>
+                        <div className='text-gray-500 md:text-[15px] text-[12px]'> {film.release_date} </div>
+                        <div className='md:text-[15px] text-[12px] pt-4'> {longText(film.overview)} </div>
+                    </div>
+                </Link>
+            )
+        )
+
+        }
+    </div>
+}
