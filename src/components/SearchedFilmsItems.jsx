c23d3848c2bf476625f665e428043f3a93e56eec
jospin6 <jospinndagano1@gmail.com>
2024-03-04 11:15:34 +0200

    Create SearchedItem.jsx

--- src/components/SearchedFilmsItems.jsx
+++ src/components/SearchedFilmsItems.jsx
@@ -1,5 +1,6 @@
-import {imageUrl, longText, emptyArray} from '../helpers/index'
+import {emptyArray} from '../helpers/index'
 import { Link } from 'react-router-dom'
+import { SearchedItem } from './SearchedItem'
 
 export const SearchedFilm = (props) => {
     const {films} = props
@@ -7,17 +8,9 @@ export const SearchedFilm = (props) => {
     return <div className="w-full h-auto py-2">
         {films.map(film => 
             (
-                <Link onClick={makeArrayEmpty()}
-                className="h-[150px] rounded-lg mb-4 itemShadow flex mx-4"
+                <Link
                 to={`/singleFilm/${film.id}`} key={film.id}>
-                    <div className="h-full w-[100px] rounded-l-lg">
-                        <img src={imageUrl(film.poster_path)} alt={film.title} className='h-full w-[100px] rounded-l-lg' />
-                    </div>
-                    <div className='px-4 pt-2'>
-                        <div className='md:text-[20px] text-[18px] font-bold'> {film.title} </div>
-                        <div className='text-gray-500 md:text-[15px] text-[12px]'> {film.release_date} </div>
-                        <div className='md:text-[15px] text-[12px] pt-4'> {longText(film.overview)} </div>
-                    </div>
+                    <SearchedItem film={film} onclic={makeArrayEmpty()}/>
                 </Link>
             )
         )
---
+++ src/components/SearchedItem.jsx
@@ -0,0 +1,14 @@
+import {imageUrl, longText} from '../helpers/index'
+export const SearchedItem = (props) => {
+    const {film, onclic} = props
+    return <div onClick={onclic} className="h-[150px] rounded-lg mb-4 itemShadow flex mx-4">
+        <div className="h-full w-[100px] rounded-l-lg">
+            <img src={imageUrl(film.poster_path)} alt={film.title} className='h-full w-[100px] rounded-l-lg' />
+        </div>
+        <div className='px-4 pt-2'>
+            <div className='md:text-[20px] text-[18px] font-bold'> {film.title} </div>
+            <div className='text-gray-500 md:text-[15px] text-[12px]'> {film.release_date} </div>
+            <div className='md:text-[15px] text-[12px] pt-4'> {longText(film.overview)} </div>
+        </div>
+    </div>
+}
