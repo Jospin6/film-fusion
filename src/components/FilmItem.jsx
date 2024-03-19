2ba9864fc558f4c3053472e53b4158e1c99658e6
jospin6 <jospinndagano1@gmail.com>
2024-03-19 16:43:09 +0200

    Add object-cover in the img class

--- src/components/FilmItem.jsx
+++ src/components/FilmItem.jsx
@@ -7,8 +7,8 @@ export const FilmItem = props => {
 
 
     return <div className="rounded-lg itemShadow">
-        <div className="w-full h-[200px] bg-red-400 rounded-t-lg relative">
-            <img src={imgUrl} alt={film.title} className="w-full h-[200px] rounded-t-lg" />
+        <div className="w-full h-[200px] rounded-t-lg relative">
+            <img src={imgUrl} alt={film.title} className="w-full h-[200px] rounded-t-lg object-cover" />
             <div className="absolute text-[12px] top-[5px] left-[5px] w-[40px] h-[40px] text-white 
                 rounded-full filmsVote_averageOpacity font-bold leading-[40px] text-center ">
                 {percentageConverter(film.vote_average).concat("%")}
