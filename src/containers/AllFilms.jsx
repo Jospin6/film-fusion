293840f90f8ae48be6d57995e80aa0da617f53e5
jospin6 <jospinndagano1@gmail.com>
2024-03-19 16:33:36 +0200

    Fix tailwind class

--- src/containers/AllFilms.jsx
+++ src/containers/AllFilms.jsx
@@ -36,7 +36,7 @@ export const AllFilms = () => {
 
     return (
         <div className="order-2 md:order-1 md:col-span-6">
-            <div className=" grid grid-cols-4 gap-2 px-2 
+            <div className=" grid grid-cols-1 px-2 
             md:px-0 md:grid-cols-6 lg:grid-cols-8 md:gap-4 h-auto pt-4"> 
                 {films.map(film => (
                     <Link className="col-span-2 mb-2 md:mb-0 " to={`/singleFilm/${film.id}`} key={film.id}> 
