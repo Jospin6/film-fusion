c20a1326f42f5cf3305dcac6ccec29e4952ec54f
jospin6 <jospinndagano1@gmail.com>
2024-03-26 10:20:27 +0200

    Import the NotFound component into the App component

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -3,6 +3,7 @@ import { Body } from './Body'
 import { Routes, Route } from 'react-router-dom';
 import { SingleFilm } from './SingleFilm'
 import { Footer } from './Footer';
+import { NotFound } from '../components/NotFound'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
           <Route exact path="/" element={<Body/>} />
           <Route path="/singleFilm/:id" element={<SingleFilm/>} />
+          <Route element={<NotFound/>} />
       </Routes>
       <Footer/>
     </>
