ca9dae6604265b70d1b444031bea5a386f21c2ba
jospin6 <jospinndagano1@gmail.com>
2024-03-27 14:31:51 +0200

    Add tailwind code in NotFound.jsx file

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,6 +1,6 @@
 
 export const NotFound = () => {
-    return <div>
+    return <div className="h-[60vh] w-full text-center font-bold text-black leading-[60vh] text-[50px]">
         404 page not found
     </div>
 }
