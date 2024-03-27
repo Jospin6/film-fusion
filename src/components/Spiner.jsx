1bde94d418efce8bed1abf5e8ee6ee6f2ccc96ca
jospin6 <jospinndagano1@gmail.com>
2024-03-27 14:35:46 +0200

    Design the spiner div

--- src/components/Spiner.jsx
+++ src/components/Spiner.jsx
@@ -1,5 +1,5 @@
 export const Spiner = () => {
     return <div className="w-full h-[60vh] flex justify-center">
-        <div></div>
+        <div className="w-[50px] h-[50px]"></div>
     </div>
 }
