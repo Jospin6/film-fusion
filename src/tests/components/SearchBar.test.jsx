e9a31a2f2356774af3a200d9b1c823b280608fe1
jospin6 <jospinndagano1@gmail.com>
2024-03-27 14:43:18 +0200

    Refactore code

--- src/tests/components/SearchBar.test.jsx
+++ src/tests/components/SearchBar.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import {describe, it, expect} from "vitest";
 import { SearchBar } from "../../components/SearchBar"
 
