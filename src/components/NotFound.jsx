ec51f2e7dd57a1943ba797c38a7835645009d6ad
jospin6 <jospinndagano1@gmail.com>
2024-03-31 11:31:16 +0200

    Change the error 404 color in red color

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,6 +1,6 @@
 
 export const NotFound = () => {
-    return <div className="h-[60vh] w-full text-center font-bold text-black leading-[60vh] text-[50px]">
+    return <div className="h-[60vh] w-full text-center font-bold text-red-500 leading-[60vh] text-[50px]">
         404 page not found
     </div>
 }
